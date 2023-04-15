1c6e6b616c5cccef7f5114abc5e4d4fdbbba8e7c
Hugo Raphael Demeyere <Hugo-Raphael.Demeyere@grenoble-inp.org>
2023-04-15 21:45:04 +0900

    feat(nexth): Added little rotation when cards mount

--- nexth/src/components/textCard.tsx
+++ nexth/src/components/textCard.tsx
@@ -1,4 +1,6 @@
 import { Card, CardHeader, Heading, CardBody, Text } from '@chakra-ui/react'
+import styles from '@/styles/components/card.module.css'
+import { useState, useEffect } from 'react'
 
 interface TextCardProps {
   title: string
@@ -8,6 +10,12 @@ interface TextCardProps {
 }
 
 const TextCard = (props: TextCardProps) => {
+  const [showCard, setShowCard] = useState(false)
+
+  useEffect(() => {
+    setShowCard(true)
+  }, [])
+
   return (
     <Card
       minWidth={'20vw'}
@@ -16,10 +24,11 @@ const TextCard = (props: TextCardProps) => {
       borderRadius={'2xl'}
       boxShadow={'0px 20px 10px rgba(0, 0, 0, 0.3)'}
       transition={'transform 0.2s ease-in-out'}
-      _hover={!props.disabled ? { transform: 'translateY(-10px)', backgroundColor: '#EDF2F7' } : {}}
+      _hover={!props.disabled ? { transform: 'translateY(-10px) rotate(3deg)', backgroundColor: '#EDF2F7' } : {}}
       onClick={() => props.onClick()}
-      style={props.disabled ? { backgroundColor: 'lightgray', opacity: 0.8, position: 'relative' } : {}}>
-      <CardHeader>
+      style={props.disabled ? { backgroundColor: 'lightgray', position: 'relative' } : {}}
+      animation={showCard ? `${styles.growRotate} 0.5s linear` : ''}>
+      <CardHeader opacity={props.disabled ? 0.6 : 1}>
         <Heading size="md"> {props.title} </Heading>
       </CardHeader>
       <CardBody>
@@ -36,13 +45,14 @@ const TextCard = (props: TextCardProps) => {
               justifyContent: 'center',
               alignItems: 'center',
               transform: 'rotate(-30deg)',
+              zIndex: 1,
             }}>
-            <Text fontSize="2xl" fontWeight="extrabold" color="white" opacity={0.8}>
+            <Text fontSize="2xl" fontWeight="extrabold" color="white">
               COMING SOON
             </Text>
           </div>
         )}
-        <Text> {props.summary} </Text>
+        <Text opacity={props.disabled ? 0.6 : 1}> {props.summary} </Text>
       </CardBody>
     </Card>
   )
