ff87ed3146fb12859a4b2ebdffc8c9d5de931533
Quinten Bons <QuintenBons@outlook.com>
2023-04-16 00:09:48 +0900

    WCstorage

--- paymasters/lib/LibMastaa.sol
+++ paymasters/lib/LibMastaa.sol
@@ -5,4 +5,18 @@ library LibMastaa {
     struct Storage {
         uint256 value;
     }
+
+    bytes32 constant WORLDCOIN_STORAGE_POSITION = keccak256("facets.storage.worldcoin");
+
+    struct WorldCoinStorage {
+
+    }
+
+    function getWorldCoinStorage() internal pure returns (LibMastaa.Storage storage ms) {
+        bytes32 position = WORLDCOIN_STORAGE_POSITION;
+        assembly {
+            ms.slot := position
+        }
+    }
+
 }
